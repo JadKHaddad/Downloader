406a5bf2ba331dbc5900cfe28c46a4f412216d92
JadKHaddad <jadkhaddad@gmail.com>
2023-01-21 23:30:01 +0100

    Master methods extracted

--- src/actors/master.rs
+++ src/actors/master.rs
@@ -43,85 +43,113 @@ impl Handler<MasterMessage> for Master {
     fn handle(&mut self, msg: MasterMessage, ctx: &mut Context<Self>) {
         match msg {
             MasterMessage::UserInput(user_input) => {
-                println!("Received UserInputMessage");
-                self.urls.insert(user_input.url.clone(), Status::Created);
+                self.on_user_input(user_input, ctx);
+            }
+            MasterMessage::Parse(parse) => {
+                self.on_parse(parse, ctx);
+            }
+            MasterMessage::Download(download) => {
+                self.on_download(download, ctx);
+            }
+            MasterMessage::Write(write) => {
+                self.on_write(write);
+            }
+        }
+    }
+}
 
-                // start a parser
-                let parser_addr = Parser {
+impl Master {
+    fn on_user_input(
+        &mut self,
+        user_input: crate::messages::user_input::UserInput,
+        ctx: &mut Context<Master>,
+    ) {
+        println!("Received UserInputMessage");
+        self.urls.insert(user_input.url.clone(), Status::Created);
+
+        // start a parser
+        let parser_addr = Parser {
+            master_addr: ctx.address(),
+        }
+        .start();
+
+        // send the parser a parse message
+        let msg = ParseMessage {
+            url: user_input.url,
+        };
+
+        parser_addr.do_send(msg);
+    }
+
+    fn on_parse(&mut self, parse: crate::messages::master::Parse, ctx: &mut Context<Master>) {
+        match parse {
+            crate::messages::master::Parse::Success(parse_success_msg) => {
+                println!("Received ParseSuccessMessage");
+
+                // start a downloader
+                let downloader_addr = Downloader {
                     master_addr: ctx.address(),
                 }
                 .start();
 
-                // send the parser a parse message
-                let msg = ParseMessage {
-                    url: user_input.url,
+                // send the downloader a download message
+                let msg = DownloadMessage {
+                    url: parse_success_msg.url,
+                    parsed_url: parse_success_msg.parsed_url,
                 };
 
-                parser_addr.do_send(msg);
+                downloader_addr.do_send(msg);
             }
-            MasterMessage::Parse(parse) => {
-                match parse {
-                    crate::messages::master::Parse::Success(parse_success_msg) => {
-                        println!("Received ParseSuccessMessage");
-
-                        // start a downloader
-                        let downloader_addr = Downloader {
-                            master_addr: ctx.address(),
-                        }
-                        .start();
-
-                        // send the downloader a download message
-                        let msg = DownloadMessage {
-                            url: parse_success_msg.url,
-                            parsed_url: parse_success_msg.parsed_url,
-                        };
-
-                        downloader_addr.do_send(msg);
-                    }
-                    crate::messages::master::Parse::Failed(msg) => {
-                        println!("Received ParseFailedMessage");
-                        self.urls
-                            .insert(msg.url, Status::Failure(Failure::ParseFailure));
-                    }
-                }
+            crate::messages::master::Parse::Failed(msg) => {
+                println!("Received ParseFailedMessage");
+                self.urls
+                    .insert(msg.url, Status::Failure(Failure::ParseFailure));
             }
-            MasterMessage::Download(download) => {
-                match download {
-                    crate::messages::master::Download::Success(download_success_msg) => {
-                        println!("Received DownloadSuccessMessage");
+        }
+    }
 
-                        // start a writer
-                        let writer_addr = Writer {
-                            master_addr: ctx.address(),
-                        }
-                        .start();
+    fn on_download(
+        &mut self,
+        download: crate::messages::master::Download,
+        ctx: &mut Context<Master>,
+    ) {
+        match download {
+            crate::messages::master::Download::Success(download_success_msg) => {
+                println!("Received DownloadSuccessMessage");
 
-                        // send the writer a write message
-                        let msg = WriteMessage {
-                            url: download_success_msg.url,
-                            response: download_success_msg.response,
-                        };
-
-                        writer_addr.do_send(msg);
-                    }
-                    crate::messages::master::Download::Failed(msg) => {
-                        println!("Received DownloadFailedMessage");
-                        self.urls
-                            .insert(msg.url, Status::Failure(Failure::DownloadFailure));
-                    }
+                // start a writer
+                let writer_addr = Writer {
+                    master_addr: ctx.address(),
                 }
+                .start();
+
+                // send the writer a write message
+                let msg = WriteMessage {
+                    url: download_success_msg.url,
+                    response: download_success_msg.response,
+                };
+
+                writer_addr.do_send(msg);
             }
-            MasterMessage::Write(write) => match write {
-                crate::messages::master::Write::Success(write_success_msg) => {
-                    println!("Received WriteSuccessMessage");
-                    self.urls.insert(write_success_msg.url, Status::Success);
-                }
-                crate::messages::master::Write::Failed(msg) => {
-                    println!("Received WriteFailedMessage");
-                    self.urls
-                        .insert(msg.url, Status::Failure(Failure::WriteFailure));
-                }
-            },
+            crate::messages::master::Download::Failed(msg) => {
+                println!("Received DownloadFailedMessage");
+                self.urls
+                    .insert(msg.url, Status::Failure(Failure::DownloadFailure));
+            }
+        }
+    }
+
+    fn on_write(&mut self, write: crate::messages::master::Write) {
+        match write {
+            crate::messages::master::Write::Success(write_success_msg) => {
+                println!("Received WriteSuccessMessage");
+                self.urls.insert(write_success_msg.url, Status::Success);
+            }
+            crate::messages::master::Write::Failed(msg) => {
+                println!("Received WriteFailedMessage");
+                self.urls
+                    .insert(msg.url, Status::Failure(Failure::WriteFailure));
+            }
         }
     }
 }
